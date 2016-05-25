1a87215fe42ae731a6788e0af38a200a9c329251
alexfriesen <friesen.alex.home@gmail.com>
2016-05-25 20:00:42 +0200

    make this website slightly more usefull

--- js/index.js
+++ js/index.js
@@ -1,59 +1,116 @@
+var DEFAULT_QUOTE = {
+  quote: "Ohai, nice to see you",
+  author: "Alex"
+};
+
 var pissedOffCounter = 0;
+var bubble = null;
+var bubbleTimer = null;
+var currentQuote = DEFAULT_QUOTE;
+var quoteCategories = null;
+var quoteCategoriesCount = 0;
 
 $(document).ready(function() {
+  site.requestCategories();
+  site.requestQuote();
 
-	$('#me').on('click', function(event) {
-		event.preventDefault();
+  $('#me').on('click', function(event) {
+    event.preventDefault();
 
-		if (pissedOffCounter < 5) {
-			new alex.Bubble(alex.translate('hi'), $('#me'));
-			
-		} else {
-			new alex.Bubble(alex.translate('buggerOff'), $('#me'));
-		}
-   	pissedOffCounter++;
-	});
+    site.showBubble();
+  });
 
 });
 
-var alex = alex || {};
-alex.Bubble = function Bubble(text, parent) {
+var site = site || {};
+site.showBubble = function() {
 
-	var element = $('<p class="speech">' + text + '</p>');
-	$(parent).append(element);
+  site.hideBubble();
 
-	setTimeout(function() {
-		try {
-			element.remove();
-		} catch (e) {
-			// so fucking what?!
-		}
-	}, 5000);
+  bubble = $('<p class="speech">' + currentQuote.quote + '<span class="author"> - ' + currentQuote.author + '</span></p>');
+  $('#me').append(bubble);
+
+  bubbleTimer = setTimeout(function() {
+    try {
+      site.hideBubble();
+    } catch ( e ) {}
+  }, 5000);
+
+  site.requestQuote();
 };
 
-alex.translate = function(key) {
-	// set language
-	if (typeof alex.lang === 'undefined') {
-		var lang = navigator.language || navigator.userLanguage || 'en';
-		alex.lang = lang.substring(0, 2);
-	}
-	try {
-		if (typeof alex.translation[alex.lang][key] !== 'undefined') {
-			return alex.translation[alex.lang][key];
-		} else {
-			return alex.translation['en'][key];
-		}
-	} catch (e) {}
-	return '';
+site.hideBubble = function() {
+  clearTimeout(bubbleTimer);
+  if (bubble) {
+    $(bubble).remove();
+  }
 }
 
-alex.translation = {
-	de: {
-		hi: "Ohai, schön das du hier bist.",
-		buggerOff: "Jetzt reicht es aber, hau endlich ab!"
-	},
-	en: {
-		hi: "Ohai, nice that you're here.",
-		buggerOff: "Now thats enough, go away!"
-	}
+site.requestQuote = function() {
+  var url = "http://quotes.rest/qod.json";
+  if (quoteCategories !== null) {
+    var key = site.getRandomCategory();
+    url = "http://quotes.rest/qod.json?category=" + key;
+  }
+
+  site.request(url, function(data) {
+    if (data.contents && data.contents.quotes && data.contents.quotes.length > 0) {
+      currentQuote = data.contents.quotes[0];
+    } else {
+      currentQuote = DEFAULT_QUOTE;
+    }
+  });
+}
+
+site.requestCategories = function() {
+  site.request("http://quotes.rest/qod/categories.json", function(data) {
+    if (data) {
+      if (data.success && data.success.total) {
+        quoteCategoriesCount = data.success.total;
+      }
+      if (data.contents && data.contents.categories) {
+        quoteCategories = data.contents.categories;
+      } else {
+        quoteCategories = null;
+      }
+    } else {
+      quoteCategories = null;
+    }
+
+  });
+}
+
+site.request = function(url, success) {
+  var xmlHttp = null;
+  try {
+    xmlHttp = new XMLHttpRequest();
+  } catch ( e ) {
+    // Fehlerbehandlung, wenn die Schnittstelle vom Browser nicht unterstützt wird.
+  }
+  if (xmlHttp) {
+    xmlHttp.open('GET', url, true);
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4) {
+        success(JSON.parse(xmlHttp.responseText));
+      }
+    };
+    xmlHttp.send(null);
+  }
+
+  return xmlHttp;
+}
+
+site.getRandomCategory = function() {
+  var maxIteration = site.getRandomInt(0, quoteCategoriesCount);
+  var iteration = 0;
+  for (var key in quoteCategories) {
+    if (iteration === maxIteration) {
+      return key;
+    }
+    iteration++;
+  }
+}
+
+site.getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
