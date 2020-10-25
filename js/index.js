f77cc516b27926046798cb1f4b37a8b83a26c70f
Alexander Friesen <friesen.alex.home@gmail.com>
2020-10-25 19:45:53 +0100

    add shoelace

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 const DEFAULT_QUOTE = 'Ohai, nice to see you'
 
-const app = new Vue({
-  el: '#app',
-  methods: {
-    contact: () => {
-      window.location.href = "mailto:info@alexfriesen.net";
-    }
-  }
+const contactButton = document.querySelector('#contact');
+contactButton.addEventListener('click', event => {
+  window.location.href = "mailto:info@alexfriesen.net";
 });
+
+// const greet = document.querySelector('#greet');
+// const me = document.querySelector('.me');
+// me.addEventListener('click', event => {
+//   greet.show();
+// });
