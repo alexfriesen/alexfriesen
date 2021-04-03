c924d140d750d6fdf6d6390b5b3f9d4f81b8a380
Alexander Friesen <friesen.alex.home@gmail.com>
2021-04-03 11:44:23 +0200

    move retro scene to https://github.com/alexfriesen/retro

--- js/retro-audio.js
+++
@@ -1,9 +0,0 @@
-const mediaElement = document.getElementById('#audio');
-
-const context = new AudioContext();
-const gainNode = new GainNode(context);
-const delayNode = new DelayNode(context);
-const audioSource = new MediaElementAudioSourceNode(context, {
-   mediaElement,
-});
-
--- js/retro.js
+++
@@ -1,1 +0,0 @@
-import('./retro-animation.js');
