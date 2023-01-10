6a2e32e3a9e919f891bdd98e779d6c69a947ecc7
Alexander Friesen <friesen.alex.home@gmail.com>
2023-01-10 16:44:09 +0100

    update year

--- src/app.vue
+++ src/app.vue
@@ -1,18 +1,13 @@
 <template>
-  <header class="shadow">
-    <Navigation />
-  </header>
-  <main>
-    <NuxtPage />
-  </main>
-  <footer>
-    <Content class="text-center">
-      &copy; 2022 - Alexander Friesen
-    </Content>
-  </footer>
+	<header class="shadow">
+		<Navigation />
+	</header>
+	<main>
+		<NuxtPage />
+	</main>
+	<footer>
+		<Content class="text-center">
+			&copy; 2023 - Alexander Friesen
+		</Content>
+	</footer>
 </template>
-
-<script setup lang="ts">
-import Content from "./components/content.vue";
-import Navigation from "./components/navigation.vue";
-</script>
