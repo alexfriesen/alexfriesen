2c08e7f1750c837b3d310d4ba1c5142dfd534085
Alexander Friesen <friesen.alex.home@gmail.com>
2023-03-27 18:51:42 +0200

    📝 improve project pages

--- src/components/link-button.vue
+++ src/components/link-button.vue
@@ -2,11 +2,13 @@
 	<NuxtLink
 		:href="url"
 		:target="target"
-		class="inline-flex gap-1 items-center justify-evenly px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-300 bg-gray-200 hover:bg-gray-900 text-gray-800 hover:text-gray-200">
-		<Icon v-if="icon" :name="icon" class="inline w-6 h-6" />
-		<slot />
-		<span v-if="label">{{ label }}</span>
-		<Icon v-if="external" name="heroicons:arrow-top-right-on-square-20-solid" class="inline w-6 h-6" />
+		class="inline-block px-5 py-3 border border-transparent no-underline font-medium rounded-md bg-gray-200 hover:bg-gray-900 text-gray-800 hover:text-gray-200">
+		<span class="flex gap-1 items-center justify-evenly">
+			<Icon v-if="icon" :name="icon" class="w-6 h-6" />
+			<slot />
+			<span v-if="label">{{ label }}</span>
+			<Icon v-if="external" name="heroicons:arrow-top-right-on-square-20-solid" class="w-6 h-6" />
+		</span>
 	</NuxtLink>
 </template>
 
