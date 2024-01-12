c2eeb3469f58e173c91828d0fc3609f93ab9193c
Alexander Friesen <friesen.alex.home@gmail.com>
2024-01-12 12:31:24 +0100

    ✨ add article images

---
+++ src/components/article-image.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="flex align-center justify-center">
+		<figure class="m-0">
+			<NuxtPicture :src="src" :alt="alt" />
+			<figcaption v-if="$slots.default">
+				<slot />
+			</figcaption>
+		</figure>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	src: {
+		type: String,
+		required: true,
+	},
+	alt: {
+		type: String,
+		required: true,
+	},
+});
+</script>
