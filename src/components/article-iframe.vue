3f88c27149c1a1c9880a67a3b188722c3ecbfb5c
Alexander Friesen <friesen.alex.home@gmail.com>
2024-01-12 12:30:44 +0100

    ✨ add preview iframe

---
+++ src/components/article-iframe.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="flex align-center justify-center w-full aspect-video">
+		<figure class="m-0 w-full">
+			<iframe :src="src" class="w-full h-full" />
+			<figcaption v-if="$slots.default">
+				<slot />
+			</figcaption>
+		</figure>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	src: {
+		type: String,
+		required: true,
+	},
+	alt: {
+		type: String,
+		required: true,
+	},
+});
+</script>
