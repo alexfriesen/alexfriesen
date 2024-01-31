603e3d8f7ecf63cad43944d91629def560ceaddf
alexfriesen <friesen.alex.home@gmail.com>
2024-01-31 18:39:42 +0100

    📝 add article hint

---
+++ src/components/article-hint.vue
@@ -0,0 +1,7 @@
+<template>
+    <UAlert class="not-prose" color="primary" variant="soft" title="Hint" icon="i-heroicons-information-circle">
+        <template #description>
+            <slot />
+        </template>
+    </UAlert>
+</template>
