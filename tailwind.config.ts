e61f684fc7c639910c1da1154da9fe3eee8a4e25
alexfriesen <friesen.alex.home@gmail.com>
2024-01-31 18:21:41 +0100

    💄 remove quotes from code blocks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,18 @@ import type { Config } from 'tailwindcss';
 export default <Partial<Config>>{
 	theme: {
 		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						'code::before': {
+							content: '',
+						},
+						'code::after': {
+							content: '',
+						},
+					},
+				},
+			},
 			aspectRatio: {
 				auto: 'auto',
 				square: '1 / 1',
